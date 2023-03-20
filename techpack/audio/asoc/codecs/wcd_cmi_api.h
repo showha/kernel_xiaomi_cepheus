6fcf9cf0c7a11a1a7b526764f39d494eee474773
showha <93635374+showha@users.noreply.github.com>
2023-03-20 13:17:19 +0800

    Update wcd_cmi_api.h

--- techpack/audio/asoc/codecs/wcd_cmi_api.h
+++ techpack/audio/asoc/codecs/wcd_cmi_api.h
@@ -28,8 +28,10 @@ enum cmi_api_event {
 };
 
 struct cmi_api_notification {
-	enum cmi_api_event event;
-	enum cmi_api_result result;
+	// enum cmi_api_event event;
+	// enum cmi_api_result result;
+	int event;
+	int result;
 	void *message;
 };
 
