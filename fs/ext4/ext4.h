ab69dee6b03e23c31d0595c730ae7fe8d5a126a4
showha <93635374+showha@users.noreply.github.com>
2023-04-02 20:25:03 +0800

    Update ext4.h

--- fs/ext4/ext4.h
+++ fs/ext4/ext4.h
@@ -2518,7 +2518,7 @@ extern int __ext4_check_dir_entry(const char *, unsigned int, struct inode *,
 				  struct ext4_dir_entry_2 *,
 				  struct buffer_head *, char *, int,
 				  ext4_lblk_t, unsigned int);
-#define ext4_check_dir_entry(dir, filp, de, bh, buf, size, offset) \
+#define ext4_check_dir_entry(dir, filp, de, bh, buf, size, lblk, offset) \
 	unlikely(__ext4_check_dir_entry(__func__, __LINE__, (dir), (filp), \
 				(de), (bh), (buf), (size), (lblk), (offset)))
 extern int ext4_htree_store_dirent(struct file *dir_file, __u32 hash,
